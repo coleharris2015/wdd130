f91500d98c0f236a4dd8b4d77a51dc3aab0cddb5
coleharris2015 <coleharris2015@gmail.com>
2022-09-24 21:26:04 -0600

    2nd list attempt

--- introwebsite/aboutme.js
+++ introwebsite/aboutme.js
@@ -1,5 +1,5 @@
-const foodList = ["Mexican","Japanese","Chinese","American"];
+let foodList = ["Mexican","Japanese","Chinese","American"];
 
-const newList = document.createElement('ul');
+let newList = document.createElement('ul');
 newList.innerHTML = '<li>{$foodList[0]}</li><li>${myList[1]}</li>';
 document.body.appendChild(newList);
